d85b639cb75f7d65e163903d8e8dcbc52211ac48
jxhnkndl <jkroyston@gmail.com>
2023-10-12 11:16:47 -0400

    create parallax scrolling number headings

--- app/components/Section.jsx
+++ app/components/Section.jsx
@@ -1,19 +1,29 @@
-'use client'
+'use client';
 import { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 export default function Section({ data }) {
   const { number, heading, bgColor, textColor } = data;
 
   const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({ target: sectionRef });
+  const y = useTransform(scrollYProgress, [0, 1], ['-300%', '100%']);
+
+  console.log(y);
+
   return (
     <section
-      ref={sectionRef}
-      className={`min-h-screen flex justify-center items-center ${bgColor}`}
+      className={`relative min-h-screen flex justify-center items-center ${bgColor}`}
     >
-      <p className={`text-9xl font-extrabold ${textColor}`}>
-        #{number}
-      </p>
-      <h2 className={`text-7xl font-bold text-black`}>{heading}</h2>
+      <div ref={sectionRef}>
+        <motion.p
+          style={{ y: y }}
+          className={`absolute left-[50px] text-9xl font-extrabold ${textColor}`}
+        >
+          #{number}
+        </motion.p>
+        <h2 className={`text-7xl font-bold text-black`}>{heading}</h2>
+      </div>
     </section>
   );
 }
